b566d7024c7d6ae3ddc91c12e1f71559e416a0d0
yuvrajbhoite <114171411+yuvrajbhoite@users.noreply.github.com>
2022-09-23 13:11:10 +0530

    first programs
    initial commit

---
+++ src/main/java/com/java/concepts/FirstClass.java
@@ -0,0 +1,8 @@
+package com.java.concepts;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/com/java/concepts/datatype/TestInteger.java
@@ -0,0 +1,11 @@
+package com.java.concepts.datatype;
+
+public class TestInteger {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("value of a is " + a);
+        System.out.println("square of a is " + a*a);
+    }
+
+}
