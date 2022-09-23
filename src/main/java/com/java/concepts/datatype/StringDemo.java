8e9094f1a95852b5ddcad7047cef85767bd17964
yuvrajbhoite <114171411+yuvrajbhoite@users.noreply.github.com>
2022-09-23 13:26:04 +0530

    string demo

---
+++ src/main/java/com/java/concepts/datatype/StringDemo.java
@@ -0,0 +1,9 @@
+package com.java.concepts.datatype;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String name = "Yuvraj";
+        System.out.println("My name is "+name);
+    }
+}
