291c4c8fef57e894d40bce9f4016201262b54708
yuvrajbhoite <114171411+yuvrajbhoite@users.noreply.github.com>
2022-09-23 13:29:35 +0530

    Updating String demo class to print the length adding files to git ignore

--- src/main/java/com/java/concepts/datatype/StringDemo.java
+++ src/main/java/com/java/concepts/datatype/StringDemo.java
@@ -5,5 +5,7 @@ public class StringDemo {
     public static void main(String[] args) {
         String name = "Yuvraj";
         System.out.println("My name is "+name);
+
+        System.out.println("Length of the name is "+name.length());
     }
 }
